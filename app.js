274e38524a2bc7c79aa24b5a440eb81334b86d09
IanJPeek <IanJPeek@gmail.com>
2019-12-09 16:07:13 +0000

    cors middleware added

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const apiRouter = require("./routes/apiRouter");
 const {handlePSQLErrors, handleCustomErrors, handleLeftoverErrors} = require("./errors/error_handling");
 
+app.use(cors());
 app.use(express.json());
 app.use("/api", apiRouter);
 
